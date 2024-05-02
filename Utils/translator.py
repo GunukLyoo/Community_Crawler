1513d678649cb9261237d2e2d591634a05b11e14
GunukLyoo <32120377+GunukLyoo@users.noreply.github.com>
2024-05-02 23:34:12 +0900

    Update translator.py
    add Type Hints

--- Utils/translator.py
+++ Utils/translator.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 import time
 
 #텍스트를 파파고 언어 자동인식으로 한글로 번역하여 출력하는 함수
-def PaPagoTranslate(driver, text):
+def PaPagoTranslate(driver: webdriver, text: str):
     url = 'https://papago.naver.com/?sk=auto&tk=ko'
     driver.get(url)
     time.sleep(3)
@@ -16,3 +16,5 @@ def PaPagoTranslate(driver, text):
 
     resultbox = driver.find_element(By.ID, 'txtTarget')
     print(resultbox.text)
+
+#def GoogleTranslate(driver: , text):
