8e78284139b0a37915f220091638ee98061406bb
GunukLyoo <32120377+GunukLyoo@users.noreply.github.com>
2024-05-04 00:05:54 +0900

    Update Community_Crawler.py
    add translate option

--- Community_Crawler.py
+++ Community_Crawler.py
@@ -1,12 +1,14 @@
 from selenium import webdriver
 from Crawler.Crawler import Crawler
-from Utils.translator import PaPagoTranslate
+from Utils.translator import PaPagoTranslate, GoogleTranslate
 import time
 
 #트위터 사용자 페이지의 첫번째 트윗을 크롤링하여 자동으로 번역하는 프로그램이다.
 
 print("가져오고자 하는 사람의 계정 id를 입력하시오: ", end='')
 id = input()
+print("사용하고자 하는 번역기를 입력하시오(파파고: 1, 구글: 2): ", end='')
+tl = input()
 
 #url = 'https://twitter.com/suisei_hosimati'
 
@@ -18,6 +20,9 @@ driver = webdriver.Chrome(options=options)
 te = Crawler.TwitterCrawling(driver, id)
 print(te)
 
-PaPagoTranslate(driver, te)
+if tl == '1':
+    PaPagoTranslate(driver, te)
+elif tl == '2':
+    GoogleTranslate(driver, te)
 
 driver.quit()
