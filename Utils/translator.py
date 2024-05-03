cac01735da4e80cd700a5a13e6735221804ced67
GunukLyoo <32120377+GunukLyoo@users.noreply.github.com>
2024-05-03 23:52:30 +0900

    Update translator.py
    add GoogleTranslate

--- Utils/translator.py
+++ Utils/translator.py
@@ -17,4 +17,16 @@ def PaPagoTranslate(driver: webdriver, text: str):
     resultbox = driver.find_element(By.ID, 'txtTarget')
     print(resultbox.text)
 
-#def GoogleTranslate(driver: , text):
+#텍스트를 구글 언어 자동인식으로 한글로 번역하여 출력하는 함수
+def GoogleTranslate(driver: webdriver, text: str):
+    url = 'https://translate.google.co.kr/?hl=ko&sl=auto&tl=ko&op=translate'
+    driver.get(url)
+    time.sleep(3)
+    
+    textbox = driver.find_element(By.CLASS_NAME, 'er8xn')
+    textbox.send_keys(text)
+    
+    time.sleep(3)
+    
+    resultbox = driver.find_element(By.CLASS_NAME, 'ryNqvb')
+    print(resultbox.text)
